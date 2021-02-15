30b3a996568a7398e79e37be7d62ec05b7285906
Anuragbansal-4 <anurag.btcse20@pec.edu.in>
2021-02-15 19:30:27 +0530

    prime changes

--- primes.py
+++ primes.py
@@ -1,17 +1,19 @@
 # function to check if given number is prime 
-def is_prime(n):
-    cur = 2
-    while cur ** 2 <= n:
-        if n % cur == 0:
-            return false # error
-        cur++
-    return true # error
+def prime_no(n):
+	if n == 1 or n == 2:
+		return True
+	c = 0
+	for i in range( 2 , n**0.5 + 1):
+		if n % i == 0:
+			c = 1
+			return False
+			break
+	if c == 0:
+		return True
+N = int(input())
+if prime_no(N):
+	print('prime')
+else:
+	print ('no prime') 
 
 
-if __name__ == "__main__":
-    n = int(input())
-    if is_prime(n):
-        print("The number if prime!!")
-    else:
-        # correct the error here
-        print(It is not prime)
